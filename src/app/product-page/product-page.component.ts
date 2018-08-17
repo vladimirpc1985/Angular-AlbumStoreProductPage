016771fb8818479c25c63f08573afe16fed81406
Vladimir Palacios <vxp5538@c02w5566htd6mbp.amer.homedepot.com>
2018-08-17 15:42:08 -0400

    Using ngFor

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -7,14 +6,10 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
-
-  albumInfo;
   
-  constructor(private _productService: ProductService) { }
+  constructor() { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response =>
-    this.albumInfo = response);
   }
 
 }
